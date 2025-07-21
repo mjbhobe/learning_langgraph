6f14a8547610dbeb87111fdcebebe05654a178be
Manish Bhobe <mjbhobe@gmail.com>
2025-07-21 10:53:03 +0530

    Added Human in Loop Example

---
+++ code_basics/08_human_in_loop.py
@@ -0,0 +1,97 @@
+from typing import TypedDict, Annotated, Any
+from dotenv import load_dotenv
+from time import sleep
+from rich.console import Console
+from rich.markdown import Markdown
+
+from langchain.chat_models import init_chat_model
+from langchain_core.tools import tool
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
+
+from langsmith import traceable
+
+# load all environment variables
+load_dotenv(override=True)
+console = Console()
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+@tool
+def get_stock_price(symbol: str) -> float:
+    """return latest stock price for stock symbol"""
+    return {
+        "MSFT": 200.3,
+        "AAPL": 100.4,
+        "AMZN": 150.0,
+        "RIL": 87.6,
+    }.get(symbol, 0.0)
+
+
+@tool
+def buy_stock(symbol: str, quantity: int) -> str:
+    """buy stock with given symbol and quantity"""
+    stock_price = get_stock_price(symbol)
+    total_price = stock_price * quantity
+    return f"You bought {quantity} shares of {symbol} (at {stock_price:.2f}/share) for {total_price:.2f}."
+
+
+tools = [get_stock_price, buy_stock]
+
+# instantiate your llm & bind with tools
+llm = init_chat_model("google_genai:gemini-2.5-flash", temperature=0.0)
+llm_with_tools = llm.bind_tools(tools)
+memory = MemorySaver()
+
+
+def chatbot(state: State) -> State:
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+# build our graph
+builder = StateGraph(State)
+builder.add_node("chatbot", chatbot)
+builder.add_node("tools", ToolNode(tools))
+# build edges: START -> chatbot -> END
+builder.add_edge(START, "chatbot")
+builder.add_conditional_edges("chatbot", tools_condition)
+# loop back to chatbot from tools node
+builder.add_edge("tools", "chatbot")
+builder.add_edge("chatbot", END)
+graph = builder.compile(checkpointer=memory)
+
+
+@traceable
+def run_chatbot(prompt: str, config=None) -> dict[str, Any] | Any:
+    message = {"role": "user", "content": prompt}
+    if config is not None:
+        response = graph.invoke({"messages": [message]}, config=config)
+    else:
+        response = graph.invoke({"messages": [message]})
+
+    return response
+
+
+# set config for memory
+config = {"configurable": {"thread_id": "abc@123"}}
+
+# Step 1: check price of AMZN stock
+prompt: str = "What is the latest price for AMZN stock? Return just the price."
+response = run_chatbot(prompt, config)
+stock_price = response["messages"][-1].content
+console.print(f"Current price of AMZN is {stock_price} per share", style="cyan")
+# deliberate call to prevent rate limit errors
+sleep(1)
+
+# Step 2: buy the stock
+prompt: str = "Buy 25 stock of AMZN"
+response = run_chatbot(prompt, config)
+console.print(response["messages"][-1].content, style="cyan")
+# deliberate call to prevent rate limit errors
+sleep(1)
